2f1c67620e8d0ce8c67f5d93e63a6cfbc8f177f6
VA_24 <60634457+VA-24@users.noreply.github.com>
2022-07-27 16:08:49 -0500

    Update loRaData.jl

--- loRaData.jl
+++ loRaData.jl
@@ -2,20 +2,21 @@ using CSV, DataFrames
 
 raw_df = DataFrame()
 
+#works
 function checkLatLong(list)
-    GPGGA = 0
-    PM = 0
+    GPGGA = 1
+    PM = 1
     for i in 1:length(list)
         if occursin("GPGGA", list[i])
             GPGGA = i
-        elseif occursin(IPS7100, list[i])
+        elseif occursin("IPS7100", list[i])
             PM = i
         else
             continue
         end
     end
 
-    if GPGGA == 0
+    if GPGGA == 0 && PM == 0
         return false
     else
         return true, GPGGA, PM
@@ -23,34 +24,44 @@ function checkLatLong(list)
 
 end
 
-mqtt_dir = readdir("C:/Users/va648/VSCode/MINTS-Variograms/data/rawMqttMFS/")
+#only reads june data for now
+mqtt_dir = readdir("D:/rawMqttMFS/")
 for elm in mqtt_dir
-    elm_path = "C:/Users/va648/VSCode/MINTS-Variograms/data/rawMqttMFS/" * elm
+    elm_path = "D:/rawMqttMFS/" * elm
     elm_dir = readdir(elm_path)
-
     if length(elm_dir) == 0
         continue
     else
         for file in elm_dir
             if file == "2022"
-                month_path = elm_path * "2022/" 
-                month_dir = readdir(month_path)
-                if "6" in month_dir
+                year_path = elm_path * "/2022/" 
+                year_dir = readdir(year_path)
+                if "06" in year_dir
+                    month_path = year_path * "06/"
+                    month_dir = readdir(month_path)
                     for date in month_dir
-                        date_path = month_path * "6/" * date * "/"
+                        date_path = month_path *  date * "/"
                         date_dir = readdir(date_path)
-
-                        if checkLatLong(date_dir)[1]
-                            gps_df = CSV.read(checkLatLong[1], DataFrame)
-                            if 30<gps_df[!, 2][1]<33 && -98<gps_df[!, 3][1]<-94
-                                pm_df = CSV.read(checkLatLong[2], DataFrame)
-                                append!(raw_df, pm_df)
+                        if length(date_dir) == 0
+                            continue
+                        else
+                            if checkLatLong(date_dir)[1]
+                                gps_df = CSV.read(date_path * date_dir[checkLatLong(date_dir)[2]], DataFrame)
+                                if 30<gps_df[!, 2][1]<33 && -98<gps_df[!, 3][1]<-94
+                                    pm_df = CSV.read(date_path * date_dir[checkLatLong(date_dir)[3]], DataFrame)
+                                    if length(names(pm_df)) == 15
+                                        append!(raw_df, pm_df)
+                                    end
+                                end
                             end
                         end
                     end
                 end
             end
-            
-            end
+        end
     end
 end
+
+#testing
+print(raw_df)
+CSV.write("C:/Users/va648/VSCode/MINTS-Variograms/data/juneData.csv", raw_df)
